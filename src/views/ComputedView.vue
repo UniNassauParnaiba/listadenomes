5d861f1c143d2b962c7f4aed09f7b9e76869a03a
Caju Tec <gestao@cajusistemas.com.br>
2026-09-11 20:59:24 -0300

    usando computed

---
+++ src/views/ComputedView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Propriedades computadas</h1>
+        <p>Has published books:</p>
+        <br>{{ publishedBooksMessage }}</br>
+        <br>{{ publishedBooksMessage }}</br>
+        <br>{{ publishedBooksMessage }}</br>
+        <br>{{ publishedBooksMessage }}</br>
+        <br>{{ publishedBooksMessage }}</br>
+        <br>{{ publishedBooksMessage }}</br>
+    </div>
+</template>
+
+<script setup>
+import { reactive, computed } from 'vue';
+
+const author = reactive({
+  name: 'John Doe',
+  books: [
+    'Vue 2 - Advanced Guide',
+    'Vue 3 - Basic Guide',
+    'Vue 4 - The Mystery'
+  ]
+})
+
+const publishedBooksMessage = computed(() => {
+  return author.books.length > 3 ? 'Yes' : 'No'
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
